b51b7676e0e0664c05807ece572c58bd7f6078d5
Daniel Atuma <atumadann@gmail.com>
2020-11-05 17:03:32 +0100

    Added route to add avatar and delete avatar

--- src/index.js
+++ src/index.js
@@ -9,11 +9,25 @@ const port = process.env.PORT || 3000
 
 const multer = require('multer')
 const upload = multer({
-    dest: 'images'
+    dest: 'images',
+    limits: {
+        fileSize: 1000000
+    },
+    fileFilter(req, file, cb) {
+        if (!file.originalname.match(/\.(doc|docx)$/)) {
+            return cb(new Error('Please upload a Word document'))
+        }
+
+        cb(undefined, true)
+    }
 })
 app.post('/upload', upload.single('upload'), (req, res) => {
     res.send()
+}, (error, req, res, next) => {
+    res.status(400).send({error: error.message})
 })
+
+
 
 app.use(express.json())
 app.use(userRouter)
@@ -22,5 +36,3 @@ app.use(taskRouter)
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
 })
-
-
